dfab6331fbb259436e570573ed93d52b1b074c61
weiwanxi <weiwanx@163.com>
2020-05-10 16:56:06 +0800

    [add] 增加user用户服务

---
+++ trade-shoes-user/src/test/java/com/shoes/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoes.user;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
