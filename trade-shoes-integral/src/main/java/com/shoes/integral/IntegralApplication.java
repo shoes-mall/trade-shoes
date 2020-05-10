0a583fa1dc9787254ade848a85e79c844b782e6b
weiwanxi <weiwanx@163.com>
2020-05-10 17:20:55 +0800

    [add] 增加integral积分服务

---
+++ trade-shoes-integral/src/main/java/com/shoes/integral/IntegralApplication.java
@@ -0,0 +1,13 @@
+package com.shoes.integral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegralApplication.class, args);
+	}
+
+}
---
+++ trade-shoes-integral/src/test/java/com/shoes/integral/IntegralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoes.integral;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
