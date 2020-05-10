2e05d62a719a4809cc68d424a47ed9fc1485c72d
weiwanxi <weiwanx@163.com>
2020-05-10 17:05:02 +0800

    [add] 增加capital资金服务

---
+++ trade-shoes-capital/src/main/java/com/shoes/capital/CapitalApplication.java
@@ -0,0 +1,13 @@
+package com.shoes.capital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalApplication.class, args);
+    }
+
+}
---
+++ trade-shoes-capital/src/test/java/com/shoes/capital/CapitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoes.capital;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
