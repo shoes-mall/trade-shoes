1708201720a1483296cdb33c9d69f5d3c60295ab
weiwanxi <weiwanx@163.com>
2020-05-10 17:22:19 +0800

    [add] 增加poundage手续费清算服务

---
+++ trade-shoes-poundage/src/main/java/com/shoes/poundage/PoundageApplication.java
@@ -0,0 +1,13 @@
+package com.shoes.poundage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoundageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoundageApplication.class, args);
+    }
+
+}
---
+++ trade-shoes-poundage/src/test/java/com/shoes/poundage/PoundageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoes.poundage;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoundageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
