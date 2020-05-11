d471d2e26430b91796d633bac4f0495fb1ac3021
bingfeng <298546lbt>
2020-05-11 15:35:22 +0800

    新增商品模块-增加模块说明

---
+++ trade-shoes-product/src/main/java/com/shoes/product/ProductApplication.java
@@ -0,0 +1,18 @@
+package com.shoes.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: TODO 描述
+ * @author: bingfeng
+ * @create: 2020-05-11 15:34
+ */
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
---
+++ trade-shoes-product/src/test/java/com/shoes/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoes.order;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
